214d5d3c37ef126dbe8c35f4dc3581f67413ef15
OskarKarlsson <oskar-lsson@github.com>
2025-10-22 16:15:43 +0200

    add members in dataManager.h

--- sensorDataAnalytics/sensorDataAnalytics/dataManager.h
+++ sensorDataAnalytics/sensorDataAnalytics/dataManager.h
@@ -1,5 +1,29 @@
 #pragma once
-class dataManager
+
+#include <string>
+#include <vector>
+#include "measurement.h"
+
+class DataManager
 {
+private:
+	std::vector<Measurement> measurement;
+public:
+	//adding measurments
+	void addMeasurement(const Measurement& m);
+	
+	//Statistics
+	float calculateMean() const;
+	float findMin() const;
+	float findMax() const;
+	float calculateStdDeviation() const;
+
+	//FILE Manager
+	void loadFromFile(const std::string& filename);
+	void saveToFile(const std::string& filename);
+
+	//printing data
+	void printData() const;
+
 };
 
