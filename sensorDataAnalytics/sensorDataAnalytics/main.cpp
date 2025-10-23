bce37ce4783cef6629f93d245b3af99669609b1d
OskarKarlsson <oskar-lsson@github.com>
2025-10-23 15:55:27 +0200

    add user input

--- sensorDataAnalytics/sensorDataAnalytics/main.cpp
+++ sensorDataAnalytics/sensorDataAnalytics/main.cpp
@@ -2,7 +2,52 @@
 //
 
 #include <iostream>
+#include "dataManager.h"
+#include "measurement.h"
 
 int main()
 {
+	DataManager tempSensor;
+	std::string time;
+	float val;
+	bool testloop = true;
+		int userChoice; int iterations{};
+	while (testloop)
+	{
+
+		int menuChoice;
+		std::cout << ""
+			"\n_______MENU_______\n"
+			"\n[1] Add new values "
+			"\n[2] Display statistics "
+			"\n[3] Exit" << std::endl;
+		std::cout << "Menu choice: "; std::cin >> menuChoice;
+		switch (menuChoice)
+		{
+		case 1: 
+			std::cout << "\nHow many measurement do you wish to enter? "; std::cin >> userChoice;
+			for (int i = 0; i < userChoice; i++)
+			{
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // rensar ev. buffert
+				std::cout << "\nTimestamp (YYYY-MM-DD HH:MM): "; std::getline(std::cin, time);
+				std::cout << "\nValue: "; std::cin >> val;
+				tempSensor.addMeasurement({ time, val });
+			}
+			break;
+		case 2: 
+			tempSensor.printData();
+			std::cout << "mean: " << tempSensor.calculateMean() << std::endl;
+			std::cout << "min: " << tempSensor.findMin() << std::endl;
+			std::cout << "max: " << tempSensor.findMax() << std::endl;
+			std::cout << "standard diviation: " << tempSensor.calcStandardDeviation() << std::endl;
+			break;
+		case 3:
+			exit(0);
+		default:
+			std::cerr << "\nERROR: Menu not found" << std::endl;
+			break;
+
+		}
+	}
+	return 0;
 }
