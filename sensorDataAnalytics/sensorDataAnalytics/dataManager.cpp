a61dbe7eb83d3106a99421a472247dcf7aaad460
Oskar Karlsson <235730477+oskar-lsson@users.noreply.github.com>
2025-10-24 09:11:45 +0200

    Update dataManager.cpp
    Add return; in loadfromfile so it skips everything if the file couldn't be opened

--- sensorDataAnalytics/sensorDataAnalytics/dataManager.cpp
+++ sensorDataAnalytics/sensorDataAnalytics/dataManager.cpp
@@ -93,6 +93,7 @@ void DataManager::loadFromFile(const std::string& filename) {
 	std::ifstream file(filename);
 	if (!file.is_open()) {
 		std::cerr << "ERROR: Failed to open " << filename << std::endl;
+  return;
 	}
 
 	std::string line;
